176ffc2d9a1443368b5d8d561be61d2eda52d457
betop <betopbr@outlook.com>
2020-11-23 15:08:39 -0300

    Changed the HTML for the site.

--- mysite2/mysite2/urls.py
+++ mysite2/mysite2/urls.py
@@ -18,11 +18,12 @@ Including another URLconf
 
 # Uncomment next two lines to enable admin:
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     # Uncomment the next line to enable the admin:
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
 
 #Use include() to add paths from the catalog application 
